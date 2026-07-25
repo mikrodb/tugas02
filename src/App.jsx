9fb0534e4cafeb6133104eafa4346cf0f7561076
sandi_mikrodb <sandisaputro54@gmail.com>
2026-07-25 11:34:03 +0700

    feat: add Home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@
 // import viteLogo from "./assets/vite.svg";
 // import heroImg from "./assets/hero.png";
 // import "./App.css";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+// import Navbar from "./components/Navbar";
+// import Hero from "./components/Hero";
+import Home from "./pages/Home";
 
 function App() {
-  // const [count, setCount] = useState(0);
   return (
     <>
-      <Navbar />
-      <Hero />
+      {/* <Navbar />
+      <Hero /> */}
+      <Home />
     </>
   );
 }

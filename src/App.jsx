0d2e011359888cca14ea3e0796ed6be5f122a0fb
sandi_mikrodb <sandisaputro54@gmail.com>
2026-07-25 11:13:03 +0700

    refactor: extract Navbar into reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -2,53 +2,14 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "./assets/vite.svg";
 // import heroImg from "./assets/hero.png";
-import "./App.css";
+// import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   // const [count, setCount] = useState(0);
   return (
     <>
-      {/* <!-- Navbar --> */}
-
-      <nav className=" bg-[#1C2127] w-full py-5">
-        <div className="wrapper max-w-7xl mx-auto flex items-center justify-between">
-          {/* <!-- Logo --> */}
-          <div className="logo-nav flex items-center gap-25">
-            <div className="logo font-bebas text-[22px] text-white">
-              <a href="#">SINEMATA</a>
-            </div>
-
-            {/* <!-- Navigation --> */}
-            <ul className="nav-links font-inter text-sm text-[#9CA3AF] flex gap-7">
-              <li>
-                <a href="#" className="hover:text-white">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-white">
-                  Browse
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-white">
-                  Watchlist
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-white">
-                  My Reviews
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          {/* <!-- Profile --> */}
-          <div className="profile w-[36px] h-[36px] bg-[#FF8000] rounded-full"></div>
-        </div>
-      </nav>
-
-      {/* <Navbar /> */}
+      <Navbar />
 
       {/* <!-- Hero --> */}
       <div className="mt-4 rounded-lg p-12 max-w-7xl mx-auto bg-linear-to-br from-[#2A1810] via-[#1A1F2E] to-[#0F1419] flex flex-col">
@@ -79,11 +40,11 @@ function App() {
             ▶ Lihat Detail
           </button> */}
 
-          <button className="btn py-5.75 px-5 bg-amber-600 rounded-lg flex items-center gap-1">
+          <button className="btn py-5 px-5 bg-amber-600 rounded-lg flex items-center gap-1">
             <span className="material-icons-round"> play_arrow </span> Lihat
             Detail
           </button>
-          <button className="btn py-5.75 px-5 bg-white/10 rounded-lg">
+          <button className="btn py-5 px-5 bg-white/10 rounded-lg">
             + Watchlist
           </button>
         </div>
